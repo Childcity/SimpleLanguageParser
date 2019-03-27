3f31911b751f8a8280411fccb3b3bf39564e8130
childcity <skulazkiy@gmail.com>
2019-03-27 19:39:37 +0200

    countMathExpr

--- SyntacticAnalyzerRecursiveDown/reversepolishnotationbuilder.h
+++ SyntacticAnalyzerRecursiveDown/reversepolishnotationbuilder.h
@@ -1,6 +1,7 @@
 #ifndef REVERSEPOLISHNOTATIONBUILDER_H
 #define REVERSEPOLISHNOTATIONBUILDER_H
 
+#include "cmath"
 #include "ASTree/astnode.h"
 
 #include <QJsonDocument>
@@ -42,24 +43,28 @@ private:
                                                //, {"token", LangTokens::GetToken(ch->getType())}
                                                , {"id", ch->getUniqueName().split("node")[1]}
                                                , {"polish", findRecurseRPN(ch)}
+                                               , {"result", (ch->getType() == Token::NUMBER)
+                                                  ? ch->getText()
+                                                  : (ch->getType() == Token::IDENT) ? ch->getText()/*Should take value of Variable from table*/: "0" }
                                            });
                 childs += ch->getText() + "(" + ch->getUniqueName().split("node")[1]/*LangTokens::GetToken(ch->getType())*/ + ") ";
             }
 
             polishTree += QVariantMap({
-                                           {  "sub_rules", subTokenLst}
-                                           , {"rule", subRule->getText()}
-                                           , {"id", subRule->getUniqueName().split("node")[1]}
-                                           , {"polish", RPNForRule(subRule, newSubRulesLst)}
+                                          {  "sub_rules", subTokenLst}
+                                          , {"rule", subRule->getText()}
+                                          , {"id", subRule->getUniqueName().split("node")[1]}
+                                          , {"polish", RPNForRule(subRule, newSubRulesLst)}
+                                          , {"result", countMathExpr(subRule, newSubRulesLst)}
                                        });
-            //qDebug().noquote() <<tab <<"Childrens: " <<childs;
-            //qDebug().noquote() <<tab<<"Was Children OF:" <<subRule->getText()  << "(" <<subRule->getUniqueName().split("node")[1] << ") "<<endl;
-
+            qDebug().noquote() <<tab <<"Childrens: " <<childs;
+            qDebug().noquote() <<tab<<"Was Children OF:" <<subRule->getText()  << "(" <<subRule->getUniqueName().split("node")[1] << ") "<<endl;
         }
 
         subRuleLst.append(subRule);
 
     }
+
     QString RPNForRule(ASTNode::SharedPtr &subToken, QList<ASTNode::SharedPtr> &subRulesLst){
         QString ruleRpn;
         Token subRuleType = subToken->getType();
@@ -110,9 +115,9 @@ private:
                 QString blockRPN = findRecurseRPN(subRulesLst.at(4));
                 return " " + assigmentRPN + " " + expr1RPN + " " + expr2RPN + " " + logicExprRPN + " " + blockRPN + " ";
             }
+        default:
+            return QString();
         }
-
-        return QString();
     }
 
     QString findRecurseRPN(ASTNode::SharedPtr rule){
@@ -124,6 +129,45 @@ private:
                 return child.toMap()["polish"].toString();
         }
         return "NOT_FOUND";
+    }
+
+    QString countMathExpr(ASTNode::SharedPtr &subToken, QList<ASTNode::SharedPtr> &subRulesLst){
+        Token subRuleType = subToken->getType();
+
+        if(subRulesLst.size() != 2) //if operators are not 2 this is error? so just return 0s
+            return QString();
+
+        if(subRulesLst.at(0)->getType() == Token::IDENT
+                || subRulesLst.at(1)->getType() == Token::IDENT)
+            return subRulesLst.at(0)->getText()+subToken->getText()+subRulesLst.at(1)->getText();
+
+        int leftOperRPN = findRecurseRPNRes(subRulesLst.at(0));
+        int rightOperRPN = findRecurseRPNRes(subRulesLst.at(1));
+        switch (subRuleType) {
+            case Token::Add:
+                return QString::number(leftOperRPN + rightOperRPN);
+            case Token::Sub:
+                return QString::number(leftOperRPN - rightOperRPN);
+            case Token::Mul:
+                return QString::number(leftOperRPN * rightOperRPN);
+            case Token::Div:
+                return QString::number(leftOperRPN / rightOperRPN);
+            case Token::Power:
+                return QString::number(pow(leftOperRPN, rightOperRPN));
+            default:
+                return QString();
+        }
+    }
+
+    int findRecurseRPNRes(ASTNode::SharedPtr rule){
+        if(rule->getType() == Token::NUMBER)
+            return rule->getText().toInt();
+
+        for(const auto &child : polishTree){
+            if(child.toMap()["id"] == rule->getUniqueName().split("node")[1])
+                return child.toMap()["result"].toInt();
+        }
+        return 0;
     }
 
 private:
